624efcf3b4c206cf2eda4167f26d5c237cfb68e4
tranbenny <tran.benny123@gmail.com>
2015-08-13 15:47:23 -0700

    edited modal and configured plan for gathering information

--- public/js/loadMap.js
+++ public/js/loadMap.js
@@ -19,8 +19,8 @@ AmCharts.ready(function() {
 	map.addListener("clickMapObject", function(event) {
 		//console.log(event.mapObject.enTitle);
 		// load up a little text box with its name 
-		$('#modal-content').empty();
-		$('#modal-content').append(event.mapObject.enTitle);
+		$('#modal-body').empty();
+		$('#modal-body').append(event.mapObject.enTitle);
 		$('#myModal').modal('show');
 	});
 
@@ -28,34 +28,11 @@ AmCharts.ready(function() {
 });
 
 
-var options = {
-	type : "GET",
-	dataType : "json", 
-	success : function(result) {
-		findLoans(result);
-	}, 
-	error : function(result) {
-		console.log("Error retrieving information" + result);
-	}
-};
-
-// result will be an object with loan data
-function findLoans(result) {	
-	var loans = result.loans; // array of objects
-	loans.forEach(function(value, index, array) {
-		console.log("Please help " + value.name);
-	}); 
-
-
-};
 
 
 
 
 
-
-
-$.ajax("http://api.kivaws.org/v1/loans/newest.json", options);
 
 
 
