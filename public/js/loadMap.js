4b3e0a0b4745b39f1a7db43c86a39e43fa5b07e3
tranbenny <tran.benny123@gmail.com>
2015-08-14 14:01:35 -0700

    got first 20 lending actions

--- public/js/loadMap.js
+++ public/js/loadMap.js
@@ -1,3 +1,35 @@
+// draws the map
+// need to add circles to map and draw circles on them
+
+var loanInformation = {};
+var recentLoansURL = "http://api.kivaws.org/v1/lending_actions/recent.json";
+var loadLoanOptions = {
+	type : "GET", 
+	dataType : "json", 
+	success : function(result) {
+		console.log("Found information!");
+		var loans = result.lending_actions;
+		// process only the first 20 loaning actions
+		for (var i = 0; i < 20; i++) {
+			var loan = loans[i];
+			if (loan.lender.lender_id in loanInformation) {
+				loanInformation[loan.lender.lender_id].push(loan.loan);
+			} else {	
+				loanInformation[loan.lender.lender_id] = [loan.loan];
+			}
+		};
+		// console.log(loanInformation);
+		// save all the location information with their loan ID's
+	},
+	error : function(err) {
+		console.log("Error retrieving data" + err);
+	}
+};
+// will get object, result.lending_actions == array of objects
+
+$.ajax(recentLoansURL, loadLoanOptions);
+
+
 AmCharts.ready(function() {
 	var map = new AmCharts.AmMap();
 	map.pathToImages = "css/images/";
