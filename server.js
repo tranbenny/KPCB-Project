caf2bfccfc2bd071cb8e71927782776d94777c10
tranbenny <tran.benny123@gmail.com>
2015-08-31 13:09:39 -0700

    added port

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var mainPageRoutes = require('./app/routes/routes');
 var api = require('./app/routes/apiRoutes');
 // var bodyParser = require('body-parser');
 
+var port = process.env.PORT || 3000; 
 
 app.use(express.static(__dirname + '/public'));
 // app.use(bodyParser());
@@ -16,7 +17,9 @@ app.get('/', function(req, res) {
 	res.send('Hello World!');
 });*/
 
-var server = app.listen(3000, function() {
+
+
+var server = app.listen(port, function() {
 	var host = server.address().address;
 	var port = server.address.port;
 	console.log("App listening on port: 3000");
